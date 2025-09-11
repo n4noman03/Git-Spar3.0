a58fdd42a18f23adcb19c979081f423c625572eb
Noman Sheikh <amaansheikh013@gmail.com>
2025-09-11 13:06:37 +0530

    file added

--- prectice.java
+++ prectice.java
@@ -3,4 +3,8 @@ public class prectice {
     public static void main(String[] args) {
         System.out.println("Hellow Noman");
     }
+
+    public void m2 (){
+        System.out.println("m2");
+    }
 }
