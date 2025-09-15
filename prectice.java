633f3787137e44b85cdeaeb1e2a1fe847dc6fd31
Noman Sheikh <amaansheikh013@gmail.com>
2025-09-15 11:50:46 +0530

    change in pretice

--- prectice.java
+++ prectice.java
@@ -1,7 +1,7 @@
 public class prectice {
 
     public static void main(String[] args) {
-        System.out.println("Hellow Noman");
+        System.out.println("Hellow Noman jiiiiiiiiiii");
     }
 
     public void m2 (){
